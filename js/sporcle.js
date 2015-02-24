e5c7b984bc3eaf87ccbf8b1ef669858be554735e
screenhunter <khanna.rjt@gmail.com>
2015-02-24 09:56:00 -0500

    commit

--- js/sporcle.js
+++ js/sporcle.js
@@ -24,7 +24,7 @@ var states = [
 	"Minnesota",
 	"Mississippi",
 	"Missouri",
-	"Monatana",
+	"Montana",
 	"Nebraska",
 	"Nevada",
 	"New Hampshire",
@@ -51,7 +51,7 @@ var states = [
 	"Wyoming"
 ];
 
-var abrev = ["AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"];
+var abrev = ["AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NH","NJ","NM","NY","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"];
 
 
 var user = new Array(50);
