6d2a1034497fa3ab043550c98b7a733c110c6a34
Rajat <khanna.rjt@gmail.com>
2015-04-10 14:18:19 -0400

    commit

--- webdev/php/finder.php
+++ webdev/php/finder.php
@@ -2,8 +2,9 @@
     $letter = $_GET["letter"];
     $length = intval($_GET["length"]) - 1;
     $file = file_get_contents("../../res/enable1.txt");
-    echo $file;
-    $pattern = '/' . $letter . '[a-zA-Z]{' . $length . '}/';
+    #echo $file;
+    #$pattern = '/' . $letter . '[a-zA-Z]{' . $length . '}/';
+    $pattern = '/' . $letter + '/';	
     echo $pattern;
     preg_match_all($pattern, $file, $matches);
     echo $matches;
