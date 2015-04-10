44680e3ee5c93483760f4a39e804decdbbb0745a
Rajat <khanna.rjt@gmail.com>
2015-04-10 14:32:58 -0400

    commit

--- webdev/php/finder.php
+++ webdev/php/finder.php
@@ -2,10 +2,12 @@
     $letter = $_GET["letter"];
     $length = intval($_GET["length"]) - 1;
     $file = file_get_contents("../../res/enable1.txt");
+    $pieces = explode(" ", $file);
     #echo $file;
     #$pattern = '/' . $letter . '[a-zA-Z]{' . $length . '}/';
     $pattern = '/ ' . $letter . ' /';	
     echo $pattern;
-    preg_match_all($pattern, $file, $matches);
-    print_r($matches);
+    foreach ($pieces as $value) {
+    	echo preg_match_all($pattern, $value);
+	}
 ?>
