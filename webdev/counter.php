1ff62312d031a77c408fa44694646e82e43ade03
Rajat <khanna.rjt@gmail.com>
2015-04-21 09:13:14 -0400

    commit

--- webdev/counter.php
+++ webdev/counter.php
@@ -12,6 +12,13 @@
 		$file = fopen("../res/count.txt", "w");
 		fwrite($file, $count);
 		fclose($file);
+		$file = fopen("../res/date.txt", "r");
+		$count = fread($file,filesize("../res/date.txt"));
+		echo "Last visit at: " . ;
+		fclose($file);
+		$file = fopen("../res/date.txt", "w");
+		fwrite($file, date('m/d/Y H:i:s'));
+		fclose($file);
 	?>
 	</body>
 </html>
