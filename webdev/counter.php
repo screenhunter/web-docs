21c6c851249187bf4ef537d630313e65ee711796
Rajat <khanna.rjt@gmail.com>
2015-04-30 10:21:51 -0400

    commit

--- webdev/counter.php
+++ webdev/counter.php
@@ -7,11 +7,11 @@
 	<?php
 		$file = "../res/counter.json";
 		$array = json_decode(file_get_contents($file), true);
-		array["count"] += 1;
+		$array["count"] += 1;
 		echo "You are visitor #" . array["count"];
 		echo "Last visit at: " . array["date"];
 		fclose($file);
-		array["date"] = date('m/d/Y H:i:s');
+		$array["date"] = date('m/d/Y H:i:s');
 		file_put_contents($file, json_encode($json));
 	?>
 	</body>
