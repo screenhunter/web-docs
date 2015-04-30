9e74cd9b2c0684a422844f4c86c3cb3ceabcc27b
Rajat <khanna.rjt@gmail.com>
2015-04-30 10:20:34 -0400

    commit

--- webdev/counter.php
+++ webdev/counter.php
@@ -5,23 +5,14 @@
 	</head>
 	<body>
 	<?php
-		$file = fopen("../res/count.txt", "r");
-		$count = intval(fread($file,filesize("../res/count.txt"))) + 1;
-		echo "You are visitor #" . $count;
+		$file = "../res/counter.json"
+		$array = json_decode(file_get_contents($file), true);
+		array["count"] += 1;
+		echo "You are visitor #" . array["count"];
+		echo "Last visit at: " . array["date"];
 		fclose($file);
-		$file = fopen("../res/count.txt", "w");
-		fwrite($file, $count);
-		fclose($file);
-
-		echo nl2br("\r\n");
-
-		$file = fopen("../res/date.txt", "r");
-		$date = fread($file,filesize("../res/date.txt"));
-		echo "Last visit at: " . $date;
-		fclose($file);
-		$file = fopen("../res/date.txt", "w");
-		fwrite($file, date('m/d/Y H:i:s'));
-		fclose($file);
+		array["date"] = date('m/d/Y H:i:s');
+		file_put_contents($file, json_encode($json));
 	?>
 	</body>
 </html>
