df1cdc04bca1f24522da2127d61cf5ac8e1002f3
Rajat <khanna.rjt@gmail.com>
2015-05-14 09:27:41 -0400

    commit

--- webdev/sql_counter.php
+++ webdev/sql_counter.php
@@ -6,8 +6,7 @@
 	<body>
 	<?php
 		$db = new SQLite3("../res/counter.db");
-		$db -> exec('CREATE TABLE IF NOT EXISTS table(id INTEGER AUTO_INCREMENT PRIMARY KEY, date TEXT);
-		');
+		$db -> exec('CREATE TABLE IF NOT EXISTS table(id INTEGER AUTO_INCREMENT PRIMARY KEY, date TEXT);');
 		$result = $db -> query('SELECT id FROM table');
 		while ($row = $result->fetchArray()) {
     		var_dump($row);
