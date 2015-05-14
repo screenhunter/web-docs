711d64235c5d0c97afbba8c12c8fba1aecb10651
Rajat <khanna.rjt@gmail.com>
2015-05-14 09:35:01 -0400

    commit

--- webdev/sql_counter.php
+++ webdev/sql_counter.php
@@ -6,10 +6,7 @@
 	<body>
 	<?php
 		$db = new SQLite3("../res/counter.db");
-		$db->exec('CREATE TABLE table (id INTEGER, dat TEXT);');
-
-$stmt = $db->prepare('SELECT name FROM people ORDER BY name;');
-$result = $stmt->execute();
+		$db->exec('CREATE TABLE table (id INTEGER, thing TEXT);');
 
 while ($row = $result->fetchArray())
 {
