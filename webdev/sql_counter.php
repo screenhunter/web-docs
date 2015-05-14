aa0153d6fafccd794b02f7572f909e70bf431e5f
Rajat <khanna.rjt@gmail.com>
2015-05-14 12:11:19 -0400

    commit

--- webdev/sql_counter.php
+++ webdev/sql_counter.php
@@ -6,13 +6,24 @@
 	<body>
 	<?php
 		$db = new SQLite3("../res/counter.db");
-		$db -> exec('CREATE TABLE IF NOT EXISTS data(id INTEGER, time TEXT, PRIMARY KEY (id));
-			INSERT INTO data(time) VAlUES("hello");
+		$time = date('m/d/Y H:i:s');
+		$mod = intval(date('i')) % 2;
+		$table = "";
+		if ($mod == 1)
+			$table = "odd";
+		else
+			$table = "even";
+
+		$db -> exec('CREATE TABLE IF NOT EXISTS ' + $table + '(id INTEGER, time TEXT, PRIMARY KEY (id));
+			INSERT INTO ' + $table + '(time) VAlUES("' + time + '");
 		');
-		$result = $db -> query('SELECT time FROM data');
-		while ($row = $result->fetchArray()) {
-    		var_dump($row);
-		}
+
+		$result = $db->query('SELECT * FROM ' + $table);
+		$rows = $result->numRows();
+		$rows -= 1;
+
+		$result = $db->query('SELECT time FROM ' + $table + ' WHERE id = ' + $rows + ';');
+		echo $result;
 	?>
 	</body>
 </html>
