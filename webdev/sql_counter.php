0376f2acc825af8b21d7a7ae4c1b9d9d65f2758a
Rajat <khanna.rjt@gmail.com>
2015-05-14 09:13:20 -0400

    commit

--- webdev/sql_counter.php
+++ webdev/sql_counter.php
@@ -6,13 +6,9 @@
 	<body>
 	<?php
 		$db = new SQLite3("../res/counter.db");
-		$db -> exec('CREATE TABLE IF NOT EXISTS table(
-			id INT(6) UNSIGNED AUTO_INCREMENT,
-			date VARCHAR(30) NOT NULL,
-			PRIMARY KEY (id)
-		)');
-		$db -> exec('INSERT INTO table
-			VAlUES(' + date('m/d/Y H:i:s') + ')
+		$db -> exec('
+			CREATE TABLE IF NOT EXISTS table(id INT(6) UNSIGNED AUTO_INCREMENT, date VARCHAR(30) NOT NULL, PRIMARY KEY (id));
+			INSERT INTO table VAlUES(' + date('m/d/Y H:i:s') + ');
 		');
 		$result = $db -> query('SELECT id FROM foo');
 		while ($row = $result->fetchArray()) {
