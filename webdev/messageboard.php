c13aab150c74c63e4f32c7b496ec78d52914f9ac
Rajat <khanna.rjt@gmail.com>
2015-06-04 10:56:16 -0400

    commit

--- webdev/messageboard.php
+++ webdev/messageboard.php
@@ -33,45 +33,6 @@
 	<div id="Old">
 		<?php
 
-			if (isset($_POST["add"])) {
-
-				if (strlen($message) == 0)
-					return;
-
-				$message = rtrim($_POST['rem'], '%09%09');
-				$day = intval(rtrim($_POST['day'], '%09%09'));
-				$year = intval(rtrim($_POST['year'], '%09%09'));
-				$month = intval(rtrim($_POST['month'], '%09%09'));
-				$pri = $year*1000 + $month + $day;
-
-				$db = new SQLite3("../res/message.db");
-				$db -> exec('CREATE TABLE IF NOT EXISTS data(pri INTEGER, mess TEXT, PRIMARY KEY (pri));
-					INSERT OR REPLACE INTO data (pri, mess) VAlUES(' . $pri . ', "' . $message . '");
-				');
-
-				$result = $db->query('SELECT * FROM data ORDER BY pri');
-				while ($row = $result->fetchArray()) {
-
-					echo $row["mess"] . " " . convert($row["pri"]);
-					echo '<form action="messageboard.php" method="POST">';
-					echo '<button id = "' . $pri . '" type = "submit"> Delete </button>';
-					echo '</form>';
-					echo nl2br("\r\n");
-
-				}
-
-			} else {
-
-				$db = new SQLite3("../res/message.db");
-				$result = $db->query('SELECT pri FROM data');
-				while ($row = $result->fetchArray())
-					if (isset($_POST["".$row["pri"]])) {
-						$db -> exec('DELETE FROM data where pri = '  . $row["pri"]);
-						return;
-					}
-
-			}
-
 			function convert($value) {
 
 				$year = floor($value/1000);
@@ -118,6 +79,45 @@
 
 				return $month . " " . $day . ", " . $year;
 				
+			}
+
+			if (isset($_POST["add"])) {
+
+				if (strlen($message) == 0)
+					return;
+
+				$message = rtrim($_POST['rem'], '%09%09');
+				$day = intval(rtrim($_POST['day'], '%09%09'));
+				$year = intval(rtrim($_POST['year'], '%09%09'));
+				$month = intval(rtrim($_POST['month'], '%09%09'));
+				$pri = $year*1000 + $month + $day;
+
+				$db = new SQLite3("../res/message.db");
+				$db -> exec('CREATE TABLE IF NOT EXISTS data(pri INTEGER, mess TEXT, PRIMARY KEY (pri));
+					INSERT OR REPLACE INTO data (pri, mess) VAlUES(' . $pri . ', "' . $message . '");
+				');
+
+				$result = $db->query('SELECT * FROM data ORDER BY pri');
+				while ($row = $result->fetchArray()) {
+
+					echo $row["mess"] . " " . convert($row["pri"]);
+					echo '<form action="messageboard.php" method="POST">';
+					echo '<button id = "' . $pri . '" type = "submit"> Delete </button>';
+					echo '</form>';
+					echo nl2br("\r\n");
+
+				}
+
+			} else {
+
+				$db = new SQLite3("../res/message.db");
+				$result = $db->query('SELECT pri FROM data');
+				while ($row = $result->fetchArray())
+					if (isset($_POST["".$row["pri"]])) {
+						$db -> exec('DELETE FROM data where pri = '  . $row["pri"]);
+						return;
+					}
+
 			}
 
 		?>
