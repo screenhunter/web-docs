1476abc21f3a095c57e1270aebccb77d2bb2dda6
Rajat <khanna.rjt@gmail.com>
2015-06-17 08:58:41 -0400

    commit

--- webdev/reg.php
+++ webdev/reg.php
@@ -12,20 +12,18 @@ session_start();
 	<h1>Register for Message Board</h1>
 	<form action="reg.php" method="POST">
 		Username:
-		<input type="text" name="user"><br>
+		<input type="text" name="user" id="user" oninput="check();"><br>
 		Password:
-		<input type="password" name="pass"><br>
+		<input type="password" name="pass" id="pass" oninput="check();"><br>
 		Confirm Password:
-		<input type="password" name="conf"><br>
-		<button type="submit" name="log">Register</button>
+		<input type="password" name="conf" id="conf" oninput="check();"><br>
+		<button type="submit" name="log" disabled="true">Register</button>
 	</form>
 	<?php
 
 		$user = rtrim($_POST['user'], '%09%09');
 		$pass = rtrim($_POST['pass'], '%09%09');
 		$conf = rtrim($_POST['conf'], '%09%09');
-		if (strlen($user) == 0 || strlen($pass) == 0 || strlen($conf) == 0)
-			exit();
 
 		$db = new SQLite3("../res/login.db");
 		$db -> exec('CREATE TABLE IF NOT EXISTS data(user TEXT, pass TEXT, PRIMARY KEY (user));');
@@ -43,6 +41,17 @@ session_start();
 			echo "Passwords did not match!";
 
 	?>
+	<script type="text/javascript">
+		function check() {
 
+			a = document.getElementById("user").value;
+			b = document.getElementById("pass").value;
+			c = document.getElementById("conf").value;
+			if (a.length == 0 || b.length == 0 || c.length == 0)
+				document.getElementById("btn").disabled = true;
+			else
+				document.getElementById("btn").disabled = false;
+		}
+	</script>
 </body>
 </html>
